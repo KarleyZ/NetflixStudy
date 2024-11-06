229d9e10783eee2129d46d56d32a40101354b038
Ling0 <1355733858@qq.com>
2024-11-06 12:49:15 +0800

    Eureka转Nacos

--- order/src/main/java/com/ling/OrderApplication.java
+++ order/src/main/java/com/ling/OrderApplication.java
@@ -3,7 +3,7 @@ package com.ling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 //Netflix提供，只服务于Eureka
 //@EnableEurekaClient
